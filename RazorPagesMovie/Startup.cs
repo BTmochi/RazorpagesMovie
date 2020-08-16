11ae0ea17e83bb6a1e0ac3e5e5a5a1e041f0faff
BTmochi <botamochibtbtmochimochi@gmail.com>
2020-08-16 11:06:28 +0900

    データベース関係

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -23,6 +23,7 @@ namespace RazorPagesMovie
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // RazorPagesMovieContext オブジェクトは、データベースへの接続と、データベース レコードへの Movie オブジェクトのマッピングのタスクを処理します。
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
